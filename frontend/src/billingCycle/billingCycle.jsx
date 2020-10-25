e70210ff01f0207ebc12771ce4aaba10a2267346
Alex Pierini <alex_pierini@hotmail.com>
2020-10-25 17:44:17 -0400

    Composant BillingCycle: reformulation pour la classe

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default (props) => (
-    <div>
-        <h1>Cycles de Paiement</h1>
-    </div>
-)
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+
+class BillingCycle extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Cycles de paiement" small="Enregistrement" />
+        <Content>Cycles de paiement</Content>
+      </div>
+    );
+  }
+}
+
+export default BillingCycle;
