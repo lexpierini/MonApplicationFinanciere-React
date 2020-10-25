5caf6ceecce3b0b18ed5ac24f5f86f2ed889cedb
Alex Pierini <alex_pierini@hotmail.com>
2020-10-25 12:32:29 -0400

    Utilisation de ValueBox

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,13 +2,39 @@ import React, { Component } from "react";
 
 import ContentHeader from "../common/template/contentHeader";
 import Content from "../common/template/content";
+import ValueBox from "../common/widget/valueBox";
+import Row from "../common/layout/row";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <ContentHeader title="Tableau de bord" small="Version 1.0" />
-        <Content>Dashboard</Content>
+        <Content>
+          <Row>
+            <ValueBox
+              cols="12 4"
+              color="green"
+              icon="bank"
+              value="CAD$ 10"
+              text="Total de Crédits"
+            />
+            <ValueBox
+              cols="12 4"
+              color="red"
+              icon="credit-card"
+              value="CAD$ 10"
+              text="Total de Débits"
+            />
+            <ValueBox
+              cols="12 4"
+              color="blue"
+              icon="money"
+              value="CAD$ 0"
+              text="Valeur consolidée"
+            />
+          </Row>
+        </Content>
       </div>
     );
   }
