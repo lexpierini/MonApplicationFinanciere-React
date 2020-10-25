2a9d6302cd12b07d62d654f955207b7d5f3dbd3b
Alex Pierini <alex_pierini@hotmail.com>
2020-10-25 18:22:29 -0400

    Composant TabHeader

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -5,6 +5,7 @@ import Content from "../common/template/content";
 import Tabs from "../common/tab/tabs";
 import TabsHeader from "../common/tab/tabsHeader";
 import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
 
 class BillingCycle extends Component {
   render() {
@@ -13,8 +14,15 @@ class BillingCycle extends Component {
         <ContentHeader title="Cycles de paiement" small="Enregistrement" />
         <Content>
           <Tabs>
-            <TabsHeader></TabsHeader>
-            <TabsContent></TabsContent>
+            <TabsHeader>
+              <TabHeader label="Liste" icon="bars" target="tabList" />
+              <TabHeader label="Inclusion" icon="plus" target="tabCreate" />
+              <TabHeader label="Modification" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Exclusion" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
+            <TabsContent>
+
+            </TabsContent>
           </Tabs>
         </Content>
       </div>
