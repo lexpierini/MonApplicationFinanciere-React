c741a2d232bd1c1b003f4ae18b36423c2c593f4f
Alex Pierini <alex_pierini@hotmail.com>
2020-10-25 21:32:31 -0400

    Composant BillingCycleList

---
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class BillingCycleList extends Component {
+  render() {
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Nom</th>
+              <th>Mois</th>
+              <th>Année</th>
+            </tr>
+          </thead>
+          <tbody>
+              
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default BillingCycleList;
