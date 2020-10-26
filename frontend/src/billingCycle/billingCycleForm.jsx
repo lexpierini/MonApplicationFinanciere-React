73cdcf763c3fa964e8cf2b894e95d345fcd5125f
Alex Pierini <alex_pierini@hotmail.com>
2020-10-26 19:22:33 -0400

    Composante sommaire

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,6 +6,7 @@ import { reduxForm, Field, formValueSelector } from "redux-form";
 import { init } from "./billingCycleActions";
 import LabelAndInput from "../common/form/labelAndInput";
 import ItemList from "./itemList";
+import Summary from "./summary";
 
 class BillingCycleForm extends Component {
   render() {
@@ -39,6 +40,7 @@ class BillingCycleForm extends Component {
             cols="12 4"
             placeholder="Entrez l'année"
           />
+          <Summary credit={1000} debt={100} />
           <ItemList
             cols="12 6"
             list={credits}
