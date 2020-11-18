2b41ab7833dc450f25d928bc89bc39a83d9e2962
lexpierini <alex_pierini@hotmail.com>
2020-11-17 21:32:01 -0500

    bug backend

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -22,7 +22,7 @@ const login = (req, res, next) => {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
             const token = jwt.sign(user.toJSON(), env.authSecret, {
-                expiresIn: "1 jour"
+                expiresIn: "1 day"
             })
             const { name, email } = user
             res.json({ name, email, token })
